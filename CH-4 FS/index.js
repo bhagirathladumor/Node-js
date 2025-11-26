9eb30a1cf01169c35f20452559faaa5861b15eb0
bhagirathladumor <bhagirathladumor970@gmail.com>
2025-11-26 11:54:05 +0530

    ch-3

---
+++ CH-4 FS/index.js
@@ -0,0 +1,50 @@
+const { log } = require("console");
+const fs = require("fs");
+
+// fs.writeFileSync("./demo.txt", "hello Node js");
+
+// fs.writeFile("./demo.txt", "hello bhagirath !!!", (error) => {
+//   if (error) {
+//     console.log("Error:", error);
+//   } else {
+//     console.log("File updated successfully!");
+//   }
+// });
+
+// fs.appendFileSync(
+//   "./demo.txt",
+//   "I am student!! " + new Date().toLocaleString() + "\n"
+// );
+
+// fs.appendFile("./demo.txt","i am bhagirath!!" + new Date().toLocaleString() + "\n" , (err) => {
+//     if(err)
+//     {
+//         console.log(err);
+        
+//     }
+//     else
+//     {
+//         console.log("program succesfull!!!");
+        
+//     }
+// });
+
+
+
+
+const result = fs.readFileSync("./demo.txt","utf-8")
+    console.log(result);
+
+fs.readFile("./demo.txt","utf-8", (err , result) => {
+    if(err)
+    {
+        console.log(err);
+        
+    }
+    else
+    {
+        console.log(result);
+        
+    }
+
+})
---
+++ Ch-3 MODULES/index.js
@@ -0,0 +1,6 @@
+const {add,sub,mul}= require(`./maths`)
+
+
+console.log(add(10,20));
+console.log(sub(10,20));
+console.log(mul(10,20));
